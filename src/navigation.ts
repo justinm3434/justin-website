b270eadb1a8dc182ded1bc3828d23cff006a8a2d
Justin Milligan <justin.milligan334@gmail.com>
2026-03-04 20:12:46 -0500

    Initial commit: personal website

---
+++ src/navigation.ts
@@ -0,0 +1,23 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    { text: 'home', href: getPermalink('/') },
+    { text: 'photos', href: '/#photos' },
+    { text: 'writing', href: '/#writing' },
+    { text: 'work', href: '/#work' },
+  ],
+  actions: [],
+};
+
+export const footerData = {
+  links: [],
+  secondaryLinks: [],
+  socialLinks: [
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://linkedin.com/in/justinmilligan34' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/justin_m34' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/justin_m34' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+  ],
+  footNote: `Justin Milligan · ${new Date().getFullYear()}`,
+};
